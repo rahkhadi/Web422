374497636569fc741583225ba856bde19ab64b03
Rahkhadi <rkhadim-hussain@myseneca.ca>
2024-02-21 18:16:33 -0500

    redone

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB422 – Assignment 2
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Rahimullah Khadim Hussain Student ID: 119515229 Date: 2024/02/07
+*  Cyclic Link: https://tame-erin-whale-shoe.cyclic.app
+*
+********************************************************************************/ 
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
